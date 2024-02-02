8a3b242a88f731d274a10410277c622e10952d63
Mohammadali Rashidfarokhi <ali.farokhi1999@gmail.com>
2024-02-02 19:01:39 +0100

    Added the search Icon, fixed the nav bar based on the pre defined UI. The only thing is left is "Set the tailwind class ”container” on the div wrapping the navbar".

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,21 @@
 import { Input } from '@/lib/shadcn-components/ui/input'
-import { socialLogo, profilePlaceholder } from '@/static/images'
+import { socialLogo, profilePlaceholder, searchIcon } from '@/static/images'
 
 export const Header = () => {
- 
   return (
-    <div className={'min-h-16 bg-amber-300 flex justify-between '}>
-      <img src={socialLogo} className='w-40'/>
-      <Input className='w-80'/>
-      <img src={profilePlaceholder} className=''/> 
+    <div className={'min-h-16 bg-black flex justify-between items-center text-white px-4'}>
+      <div className="flex items-center">
+        <img src={socialLogo} className="w-40" />
+      </div>
 
-      
+      <div className="flex items-center">
+        <img src={searchIcon} className="place-items-start" />
+        <div className="flex flex-col items-center ml-2">
+          <Input className="w-80 mt-2" placeholder="Search for User" />
+        </div>
+      </div>
+
+      <img src={profilePlaceholder} className="w-16" />
     </div>
   )
 }
