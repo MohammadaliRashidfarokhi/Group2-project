6445432f15d04047eb7047d2f30cc70bc5e75124
MadihaGul <madiha.gul@yoin.tech>
2024-02-06 00:02:31 +0100

    Modified post component and added dummy posts

--- src/modules/home/view/HomePage/Post.tsx
+++ src/modules/home/view/HomePage/Post.tsx
@@ -4,6 +4,8 @@ type Props = {
     name: string
     username: string
     text: string
+    likes: number
+    comments: number
   }
 }
 
@@ -12,14 +14,27 @@ export const Post = (props: Props) => {
 
   return (
     <div className={'border'}>
-      <div className={'flex flex-col'}>
-        <div className={'font-bold text-white'}>{data.name}</div>
-        <div className={'text-gray-500'}>{data.username}</div>
+      <div className={'flex justify-end items-start mt-4 mr-4'}>
+        <img src="/dots-icon.svg" alt="More" />
       </div>
-      <p className={'mt-1 text-white'}>{data.text}</p>
-      <div className={'mt-2 text-white'}>
-        <span>Heart</span>
-        <span>Comment</span>
+      <div className={'ml-4 flex flex-row'}>
+        <img src="/user-icon.svg" alt="user" />
+        <div className={'flex flex-col'}>
+          <div className={'ml-2 font-bold text-white'}>{data.name}</div>
+          <div className={'ml-2 text-gray-500'}>{data.username}</div>
+        </div>
+      </div>
+
+      <p className={'ml-4 mt-1 text-white'}>{data.text}</p>
+      <div className={'ml-4 mt-2 text-white flex flex-row gap-2'}>
+        <span className={'mt-2 text-white flex flex-row gap-1'}>
+          <img src="/heart-icon.svg" alt="Likes" />
+          <p className={'mb-1 text-white'}>{data.likes}</p>
+        </span>
+        <span className={'mt-2 text-white flex flex-row gap-1'}>
+          <img src="/comment-icon.svg" alt="Comments" />
+          <p className={'mb-1 text-white'}>{data.comments}</p>
+        </span>
       </div>
     </div>
   )
