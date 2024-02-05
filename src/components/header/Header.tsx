c72c839750a5901d9cc85f3f199061b82c38b366
Mohammadali Rashidfarokhi <ali.farokhi1999@gmail.com>
2024-02-05 17:09:00 +0100

    Added the container

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -15,32 +15,34 @@ export const Header = () => {
   }
 
   return (
-    <div className={'min-h-16 bg-black flex justify-between items-center text-white px-4 fixed w-full top-0'}>
-      <div className="flex items-center">
-        <img src={socialLogo} className="w-40" alt="Social Logo" />
-      </div>
-
-      <div className="flex items-center">
-        <img src={searchIcon} className="place-items-start" alt="Search Icon" />
-        <div className="flex flex-col items-center ml-2">
-          <Input className="w-80 mt-2" placeholder="Search for User" />
+    <div className={'min-h-16 bg-black fixed w-full top-0'}>
+      <div className="container mx-auto px-4 flex justify-between items-center text-white">
+        <div className="flex items-center">
+          <img src={socialLogo} className="w-40" alt="Social Logo" />
         </div>
-      </div>
 
-      <div className="relative">
-        <img
-          src={profilePlaceholder}
-          className="w-16 cursor-pointer"
-          alt="Profile Placeholder"
-          onClick={toggleDropdown}
-        />
-        {isDropdownOpen && (
-          <div className="absolute right-0 mt-2 bg-white text-black shadow-md">
-            <div className="py-2 px-4 cursor-pointer" onClick={handleLogout}>
-              Logout
-            </div>
+        <div className="flex items-center">
+          <img src={searchIcon} className="place-items-start" alt="Search Icon" />
+          <div className="flex flex-col items-center ml-2">
+            <Input className="w-80 mt-2" placeholder="Search for User" />
           </div>
-        )}
+        </div>
+
+        <div className="relative">
+          <img
+            src={profilePlaceholder}
+            className="w-16 cursor-pointer"
+            alt="Profile Placeholder"
+            onClick={toggleDropdown}
+          />
+          {isDropdownOpen && (
+            <div className="absolute right-0 mt-2 bg-white text-black shadow-md">
+              <div className="py-2 px-4 cursor-pointer" onClick={handleLogout}>
+                Logout
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
