810adb9c91b433b91230e522c27b8b82f5a02bbd
EugeneDvoryankov <kolontayev1@gmail.com>
2024-02-06 16:11:27 +0100

    Refactored the Post Component to be Wrapped inside a ui.chadcn Card component and fixed the margins

--- src/modules/home/view/HomePage/Post.tsx
+++ src/modules/home/view/HomePage/Post.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent} from '@/lib/shadcn-components/ui/card.tsx';
+
 type Props = {
   data: {
     id: number
@@ -13,29 +15,31 @@ export const Post = (props: Props) => {
   const { data } = props
 
   return (
-    <div className={'border'}>
-      <div className={'flex justify-end items-start mt-4 mr-4'}>
-        <img src="/dots-icon.svg" alt="More" />
-      </div>
-      <div className={'ml-4 flex flex-row'}>
-        <img src="/user-icon.svg" alt="user" />
-        <div className={'flex flex-col'}>
-          <div className={'ml-2 font-bold text-white'}>{data.name}</div>
-          <div className={'ml-2 text-gray-500'}>{data.username}</div>
+    <Card>
+      <CardContent>
+        <div className={'flex justify-end items-start mt-4'}>
+          <img src="/dots-icon.svg" alt="More" />
         </div>
-      </div>
+        <div className={'flex flex-row'}>
+          <img src="/user-icon.svg" alt="user" />
+          <div className={'flex flex-col'}>
+            <div className={'ml-2 font-bold text-white'}>{data.name}</div>
+            <div className={'ml-2 text-gray-500'}>{data.username}</div>
+          </div>
+        </div>
 
-      <p className={'ml-4 mt-1 text-white'}>{data.text}</p>
-      <div className={'ml-4 mt-2 text-white flex flex-row gap-2'}>
+        <p className={'mt-1 mr-1 text-white'}>{data.text}</p>
+        <div className={'mt-3 text-white flex flex-row gap-2'}>
         <span className={'mt-2 text-white flex flex-row gap-1'}>
           <img src="/heart-icon.svg" alt="Likes" />
           <p className={'mb-1 text-white'}>{data.likes}</p>
         </span>
-        <span className={'mt-2 text-white flex flex-row gap-1'}>
+          <span className={'mt-2 text-white flex flex-row gap-1'}>
           <img src="/comment-icon.svg" alt="Comments" />
           <p className={'mb-1 text-white'}>{data.comments}</p>
         </span>
-      </div>
-    </div>
+        </div>
+      </CardContent>
+    </Card>
   )
 }
