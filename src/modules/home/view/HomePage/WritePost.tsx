a96de3449cdac521cca7d8e027e885d5cfb78643
EugeneDvoryankov <kolontayev1@gmail.com>
2024-02-14 17:52:20 +0100

    Completed GUI for Create post component

--- src/modules/home/view/HomePage/WritePost.tsx
+++ src/modules/home/view/HomePage/WritePost.tsx
@@ -1,24 +1,21 @@
-import { Card, CardContent, CardHeader} from '@/lib/shadcn-components/ui/card.tsx'
+import { Card, CardContent, CardHeader, CardFooter} from '@/lib/shadcn-components/ui/card.tsx'
 import {Button} from '@/lib/shadcn-components/ui/button.tsx'
 import {Input} from '@/lib/shadcn-components/ui/input.tsx'
 
-/*
-type Props = {
-  //onSubmit: (textInput: string) => void;
-}
- */
 
 export const WritePost = () => {
 
   return (
     <Card>
-      <CardContent className={'text-white relative px-7 py-5 flex flex-col gap-2 mb-2 mt-2' }>
-        <CardHeader>
-          <h3 className={'font-bold'}>Write a post</h3>
-        </CardHeader>
-        <Input placeholder="What's popping" />
-        <Button type="submit">Post</Button>
+      <CardHeader className={'pb-3 pt-3'}>
+        <h3 className={'text-white text-2xl font-bold'}>Write a post</h3>
+      </CardHeader>
+      <CardContent className={'text-white relative flex flex-col pb-2.5' }>
+        <Input className={'border-black p-0 ring-offset-black'} placeholder="What's popping..." />
       </CardContent>
+      <CardFooter className={'flex'}>
+        <Button className={'grow'} type={'submit'}>Post</Button>
+      </CardFooter>
     </Card>
   )
 }
