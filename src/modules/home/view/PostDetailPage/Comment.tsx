2d1979978627d6b4316319be6c43ab06c0df217f
Leonardo Pavarino <leonardo.pavarino@outlook.com>
2024-02-23 15:44:33 +0100

    began prettyfing work

--- src/modules/home/view/PostDetailPage/Comment.tsx
+++ src/modules/home/view/PostDetailPage/Comment.tsx
@@ -1,13 +1,38 @@
 import { CommentDetail } from "@/model/comment"
+import { Card, CardContent } from '@/lib/shadcn-components/ui/card.tsx'
+import { commentIcon, dotsIcon, heartIcon, profilePlaceholder } from '@/static/images.ts'
 
 type Props = {
     data: CommentDetail
 }
-  
+
 export const Comment = (props: Props) => {
-    const {data} = props
-    
-    return <>
-        <p>I am a comment :) {data.CONTENT}</p>
-    </>
+    const { data } = props
+
+    return <Card>
+        <CardContent className={'text-white relative px-7 py-5 flex flex-col gap-2'}>
+            <img src={dotsIcon} className={'top-6 right-6 absolute cursor-pointer'} alt="More" />
+
+            <div className={'flex flex-row gap-2'}>
+                <img src={profilePlaceholder} className={'w-10'} alt="user" />
+                <div className={'flex flex-col'}>
+                    <div className={'font-bold'}>{`${data.FIRST_NAME} ${data.LAST_NAME}`}</div>
+                    <div className={'text-gray-500'}>@{data.USERNAME}</div>
+                </div>
+            </div>
+
+            <span>{data.CONTENT}</span>
+
+            <div className={'flex flex-row gap-2 mt-2'}>
+                <span className={'flex flex-row gap-1'}>
+                    <img className={'cursor-pointer'} src={heartIcon} alt="Likes" />
+                    <span>{0}</span>
+                </span>
+                <span className={'flex flex-row gap-1'}>
+                    <img className={'cursor-pointer'} src={commentIcon} alt="Comments" />
+                    <span>{0}</span>
+                </span>
+            </div>
+        </CardContent>
+    </Card>
 }
