7c4bee88f788fc2676038025fe92491b7f0a1e9b
felixbrunnegard <44345418+felixbrunnegard@users.noreply.github.com>
2024-02-29 16:17:14 +0100

    Removed color

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ export const Layout = (props: Props) => {
   const { variant = 'full' } = props
 
   return (
-    <main className="flex w-full h-full overflow-hidden overflow-y-scroll bg-black justify-center">
+    <main className="flex w-full h-full overflow-hidden overflow-y-scroll justify-center">
       <div
         className={cn('md:w-[500px] md:py-10 flex px-4 md:px-0', {
           'md:w-[700px]': variant === 'full',
