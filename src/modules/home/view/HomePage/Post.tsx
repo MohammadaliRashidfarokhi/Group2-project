b91e5afcedd77f312fee19a2068628f34f1a459e
martinnesnera <martinnesnera00@gmail.com>
2024-03-05 15:50:14 +0100

    feat: design polishing

--- src/modules/home/view/HomePage/Post.tsx
+++ src/modules/home/view/HomePage/Post.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent } from '@/lib/shadcn-components/ui/card.tsx'
-import { commentIcon, heartIcon, profilePlaceholder } from '@/static/images.ts'
+import { commentIcon, profilePlaceholder } from '@/static/images.ts'
 import { PostDetail } from '@/model/post.ts'
-import { TrashIcon } from '@radix-ui/react-icons'
+import { HeartFilledIcon, TrashIcon } from '@radix-ui/react-icons'
 import { useState } from 'react'
 import {
   AlertDialog,
@@ -15,31 +15,31 @@ import { Button } from '@/lib/shadcn-components/ui/button.tsx'
 import { Link } from 'react-router-dom'
 import { APP_ROUTES } from '@/config/router/routes.ts'
 import { useTranslation } from '@/locales/i18n.ts'
+import { cx } from 'class-variance-authority'
+import * as dayjs from 'dayjs'
 
 type Props = {
+  isLikedByCurrentUser?: boolean
   data: PostDetail
   onRemove?: () => void
   onLikeClick?: () => void
 }
 
 export const Post = (props: Props) => {
-  const { data, onRemove, onLikeClick } = props
+  const { data, onRemove, onLikeClick, isLikedByCurrentUser = false } = props
   const [showConfirmation, setShowConfirmation] = useState(false)
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   const handleRemove = () => {
-    // Show the confirmation dialog
     setShowConfirmation(true)
   }
 
   const confirmRemove = () => {
-    // Remove post and close the confirmation dialog
     onRemove?.()
     setShowConfirmation(false)
   }
 
   const cancelRemove = () => {
-    // Cancel the removal and close the confirmation dialog
     setShowConfirmation(false)
   }
 
@@ -49,13 +49,11 @@ export const Post = (props: Props) => {
         {onRemove && (
           <AlertDialog open={showConfirmation}>
             <AlertDialogTrigger>
-              <TrashIcon className={'top-7 right-5 absolute w-5 h-5'} onClick={handleRemove}/>
+              <TrashIcon className={'top-7 right-5 absolute w-5 h-5'} onClick={handleRemove} />
             </AlertDialogTrigger>
             <AlertDialogContent>
               <AlertDialogHeader>{t('delete-post')}</AlertDialogHeader>
-              <AlertDialogDescription>
-                {t('delete-post-description')}
-              </AlertDialogDescription>
+              <AlertDialogDescription>{t('delete-post-description')}</AlertDialogDescription>
               <AlertDialogFooter>
                 <Button onClick={cancelRemove} variant={'secondary'}>
                   {t('cancel')}
@@ -68,28 +66,33 @@ export const Post = (props: Props) => {
           </AlertDialog>
         )}
 
-        <div className={'flex flex-row gap-2'}>
+        <Link to={APP_ROUTES.user(data.author)} className={'flex flex-row gap-2 w-fit'}>
           <img src={profilePlaceholder} className={'w-10'} alt="user" />
           <div className={'flex flex-col'}>
             <div className={'font-bold'}>{`${data.FIRST_NAME} ${data.LAST_NAME}`}</div>
             <div className={'text-muted-foreground'}>@{data.USERNAME}</div>
           </div>
-        </div>
+        </Link>
 
         <span>{data.CONTENT}</span>
 
-        <div className={'flex flex-row gap-3.5 mt-2'}>
-          <span className={'flex flex-row gap-1 cursor-pointer'} onClick={onLikeClick}>
-            <img className={'text-gray-500 hover:text-white'} src={heartIcon} alt="Likes" />
-            <span>{data.likes.length}</span>
-          </span>
-
-          <Link to={APP_ROUTES.comments(data.id)}>
-            <span className={'flex flex-row gap-1 cursor-pointer'}>
-              <img src={commentIcon} alt="Comments" />
-              <span>{data.comments}</span>
+        <div className={'flex flex-row justify-between items-end'}>
+          <div className={'flex flex-row gap-3.5 mt-2'}>
+            <span className={'flex flex-row gap-1 cursor-pointer items-center'} onClick={onLikeClick}>
+              <HeartFilledIcon
+                className={cx('w-5 h-5 text-gray-400 hover:text-red-500', { 'text-red-500': isLikedByCurrentUser })}
+              />
+              <span>{data.likes.length}</span>
             </span>
-          </Link>
+
+            <Link to={APP_ROUTES.comments(data.id)}>
+              <span className={'flex flex-row gap-1 cursor-pointer'}>
+                <img src={commentIcon} alt="Comments" />
+                <span>{data.comments}</span>
+              </span>
+            </Link>
+          </div>
+          <div className={'text-gray-400 text-sm'}>{dayjs(data.PUBLISHED_AT).format('YYYY-MM-DD h:m')}</div>
         </div>
       </CardContent>
     </Card>
--- src/modules/home/view/PostDetailPage/Comment.tsx
+++ src/modules/home/view/PostDetailPage/Comment.tsx
@@ -1,7 +1,7 @@
 import { CommentDetail } from '@/model/comment'
 import { Button } from '@/lib/shadcn-components/ui/button.tsx'
 import { Card, CardContent } from '@/lib/shadcn-components/ui/card.tsx'
-import { heartIcon, profilePlaceholder} from '@/static/images.ts'
+import { profilePlaceholder } from '@/static/images.ts'
 import { useState } from 'react'
 import {
   AlertDialog,
@@ -12,33 +12,34 @@ import {
   AlertDialogTrigger,
 } from '@/lib/shadcn-components/ui/alert-dialog.tsx'
 import { useTranslation } from '@/locales/i18n.ts'
-import { TrashIcon } from '@radix-ui/react-icons'
-
+import { HeartFilledIcon, TrashIcon } from '@radix-ui/react-icons'
+import { Link } from 'react-router-dom'
+import { APP_ROUTES } from '@/config/router/routes.ts'
+import { cx } from 'class-variance-authority'
+import * as dayjs from 'dayjs'
 
 type Props = {
+  isLikedByCurrentUser?: boolean
   data: CommentDetail
   onRemove?: () => void
   onLikeClick?: () => void
 }
 
 export const Comment = (props: Props) => {
-  const { data, onRemove, onLikeClick } = props
+  const { data, onRemove, onLikeClick, isLikedByCurrentUser = false } = props
   const [showConfirmation, setShowConfirmation] = useState(false)
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   const handleRemove = () => {
-    // Show the confirmation dialog
     setShowConfirmation(true)
   }
 
   const confirmRemove = () => {
-    // Remove post and close the confirmation dialog
     onRemove?.()
     setShowConfirmation(false)
   }
 
   const cancelRemove = () => {
-    // Cancel the removal and close the confirmation dialog
     setShowConfirmation(false)
   }
 
@@ -48,13 +49,11 @@ export const Comment = (props: Props) => {
         {onRemove && (
           <AlertDialog open={showConfirmation}>
             <AlertDialogTrigger>
-              <TrashIcon className={'top-7 right-5 absolute w-5 h-5'} onClick={handleRemove}/>
+              <TrashIcon className={'top-7 right-5 absolute w-5 h-5'} onClick={handleRemove} />
             </AlertDialogTrigger>
             <AlertDialogContent>
               <AlertDialogHeader>{t('delete-post')}</AlertDialogHeader>
-              <AlertDialogDescription>
-                {t('delete-post-description')}
-              </AlertDialogDescription>
+              <AlertDialogDescription>{t('delete-post-description')}</AlertDialogDescription>
               <AlertDialogFooter>
                 <Button onClick={cancelRemove} variant={'secondary'}>
                   {t('cancel')}
@@ -67,21 +66,26 @@ export const Comment = (props: Props) => {
           </AlertDialog>
         )}
 
-        <div className={'flex flex-row gap-2'}>
+        <Link to={APP_ROUTES.user(data.author)} className={'flex flex-row gap-2 w-fit'}>
           <img src={profilePlaceholder} className={'w-10'} alt="user" />
           <div className={'flex flex-col'}>
             <div className={'font-bold'}>{`${data.FIRST_NAME} ${data.LAST_NAME}`}</div>
             <div className={'text-muted-foreground'}>@{data.USERNAME}</div>
           </div>
-        </div>
+        </Link>
 
         <span>{data.CONTENT}</span>
 
-        <div className={'flex flex-row gap-3.5 mt-2'}>
-          <span className={'flex flex-row gap-1 cursor-pointer'} onClick={onLikeClick}>
-            <img className={'text-gray-500 hover:text-white'} src={heartIcon} alt="Likes" />
-            <span>{data.likes.length}</span>
-          </span>
+        <div className={'flex flex-row justify-between items-end'}>
+          <div className={'flex flex-row gap-3.5 mt-2'}>
+            <span className={'flex flex-row gap-1 cursor-pointer items-center'} onClick={onLikeClick}>
+              <HeartFilledIcon
+                className={cx('w-5 h-5 text-gray-400 hover:text-red-500', { 'text-red-500': isLikedByCurrentUser })}
+              />
+              <span>{data.likes.length}</span>
+            </span>
+          </div>
+          <div className={'text-gray-400 text-sm'}>{dayjs(data.PUBLISHED_AT).format('YYYY-MM-DD h:m')}</div>
         </div>
       </CardContent>
     </Card>
