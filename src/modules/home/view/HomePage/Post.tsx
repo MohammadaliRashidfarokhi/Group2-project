95f8b64f76cd704f449b047bd527592f248d8c5e
martinnesnera <martinnesnera00@gmail.com>
2024-03-05 23:31:26 +0100

    removal of dayjs

--- src/modules/home/view/HomePage/Post.tsx
+++ src/modules/home/view/HomePage/Post.tsx
@@ -16,6 +16,7 @@ import { Link } from 'react-router-dom'
 import { APP_ROUTES } from '@/config/router/routes.ts'
 import { useTranslation } from '@/locales/i18n.ts'
 import { cx } from 'class-variance-authority'
+import * as dayjs from 'dayjs'
 
 type Props = {
   data: PostDetail
@@ -91,6 +92,7 @@ export const Post = (props: Props) => {
               </span>
             </Link>
           </div>
+          <div className={'text-gray-400 text-sm'}>{dayjs(new Date(data.PUBLISHED_AT)).format('YYYY-MM-DD h:m')}</div>
         </div>
       </CardContent>
     </Card>
